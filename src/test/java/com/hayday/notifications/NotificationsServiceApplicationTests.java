8fc2ce77193b2a3f1269f7c308cc3fbe3729158f
Arsh Chouhan <arshchouhan004@gmail.com>
2026-05-06 00:14:44 +0530

    test: mock NotificationRepository to avoid needing MongoDB in tests

--- src/test/java/com/hayday/notifications/NotificationsServiceApplicationTests.java
+++ src/test/java/com/hayday/notifications/NotificationsServiceApplicationTests.java
@@ -1,10 +1,15 @@
 package com.hayday.notifications;
 
+import com.hayday.notifications.repository.NotificationRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class NotificationsServiceApplicationTests {
+
+    @MockBean
+    private NotificationRepository notificationRepository;
 
     @Test
     void contextLoads() {
